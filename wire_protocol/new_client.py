05a219aa05f58c7b80c75d41f897ec076867bfa3
Alyssa <alyssahuang@college.harvard.edu>
2023-02-12 02:41:09 -0500

    Single message sending works but messy

--- wire_protocol/new_client.py
+++ wire_protocol/new_client.py
@@ -1,44 +1,83 @@
 import socket
-import pickle
+import pickle # we can't use this in our final implementation according to waldo
 
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "10.250.18.31"
+SERVER = "10.250.10.42" # TODO: need to change later/have some way to make it dynamic
 ADDR = (SERVER, PORT)
+LOGIN_MESSAGE = "!LOGGEDIN"
+NO_MORE_DATA = "!NOMOREDATA"
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+class ChatClient:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(ADDR)
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
+        self.logged_in = False
+        self.username = None
+
+        while not self.logged_in:
+            # TODO: ensure that blocking happens here; fine cuz we have sep thread for each client
+            response = self.wait_for_response()
+            print(response)
+
+            # First sends a confirmation that the user has logged in, then the username
+            if LOGIN_MESSAGE in response:
+                # TODO: ITS SENDING LIKE 3 things at once need to figure that shit out
+                # TODO: partiion messages?
+                self.logged_in = True
+                print("Logged in!")
+                self.username = user_input
+                break
+            
+            user_input = input()
+            self.send(user_input)
+        
+        # Receive all missed messages
+        while NO_MORE_DATA not in response:
+            print(response)
+            response = self.wait_for_response()
+        
+        print("No more messages.")
+        # Client decides what to do after logging in
+        # print(self.wait_for_response())
+        # action = input()
+        # self.send(action)
+        # print(action)
+        # if action == "0":
+        
+        # TODO: literally zero error handling
+        self.send(self.username)
+
+        prompt = self.wait_for_response()
+        print(prompt)
+
+        recipient = input()
+        self.send(recipient)
+
+        prompt = self.wait_for_response()
+        print(prompt)
+
+        message = input()
+        self.send(message)
+
+        print("Message sent!")
+
+    def send(self, msg):
+        message = msg.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+        self.client.send(send_length)
+        self.client.send(message)
     
-    data = client.recv(2048).decode(FORMAT)
-    print(data)
-    return data
+    def wait_for_response(self):
+        data = self.client.recv(2048).decode(FORMAT)
+        return data
 
-logged_in = False
-start = client.recv(2048).decode(FORMAT)
-print("start", start)
-
-if start == "What's your username?":
-    while not logged_in:
-        username = input()
-        data = send(username)
-
-        if data == "Logged in!":
-            logged_in = True
-
-
-users = client.recv(2048)
-users_arr = pickle.loads(users)
-print(users_arr)
+chat_client = ChatClient()
 
 # send a message - maybe commands of who to send a message to
 
--- wire_protocol/new_server.py
+++ wire_protocol/new_server.py
@@ -5,25 +5,25 @@ import pickle
 # bytes for the metadata of how long the message is, then cater the actual size
 HEADER = 64
 PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
+SERVER = socket.gethostbyname(socket.gethostname()) # TODO: WHAT IS THIS?
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
+LOGIN_MESSAGE = "!LOGGEDIN"
+NO_MORE_DATA = "!NOMOREDATA"
+SPLIT_MESSAGE = "#" # need to change this
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+class ChatServer:
+    def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind(ADDR)
 
-# before we queue it onto the message 
-messages = []
-accounts = {}
+        # TODO: will need to use a mutex lock for each of these likely
+        self.unsent_messages = {} # {username: [msg1, msg2, msg3]}
+        self.accounts = [] # [username1, username2, username3]
+        self.active_accounts = {} # {username: addr}
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
-    logged_in = False
-    current_user = None
-
-    # establish username
-    while not logged_in:
+    def login_user(self, conn, addr):
         conn.send("What's your username?".encode(FORMAT))
         msg_length = conn.recv(HEADER).decode(FORMAT)
         msg_length = int(msg_length)
@@ -31,47 +31,136 @@ def handle_client(conn, addr):
         
         print(f"[{addr}] {username}")
 
-        if username in accounts.keys():
-            conn.send("Username already exists. What's your username?".encode(FORMAT))
-            print("username already exists")
+        if username not in self.accounts:
+            conn.send("Username does not exist.".encode(FORMAT))
         else:
-            accounts[username] = addr
-            current_user = username
-            logged_in = True
-            conn.send("Logged in!".encode(FORMAT))
-
-
-    # send current users
-    print("current users")
-
-    data_string = pickle.dumps(list(accounts.keys()))
-    conn.send(data_string)
-
-
-    connected = True
-    while connected:
-        # depends on your protocol
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-            print(f"[{addr}] {msg}")
-            conn.send("Msg received".encode(FORMAT))
-
-    conn.close()
+            # Log in user
+            self.active_accounts[username] = addr
         
+        return username
 
-def start():
-    server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+    def register_user(self, conn, addr):
+        conn.send("What's your username?".encode(FORMAT))
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+        username = conn.recv(msg_length).decode(FORMAT)
+        
+        print(f"[{addr}] {username}")
 
+        if username in self.accounts:
+            conn.send("Username already exists.".encode(FORMAT))
+        else:
+            # Register and log in user
+            self.active_accounts[username] = addr
+            self.accounts.append(username)
+            self.unsent_messages[username] = []
+        
+        return username
 
+    def record_chat_message(self, conn, addr):
+        # Block until we get the sender's username
+        sender = conn.recv(2048).decode(FORMAT)
+
+        print(sender)
+
+        # TODO: we should lowkey wrap this in a function LOL
+        conn.send("Who do you want to send a message to?".encode(FORMAT))
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+        recipient = conn.recv(msg_length).decode(FORMAT)
+
+        if recipient not in self.accounts:
+            conn.send("User does not exist.".encode(FORMAT))
+            # TODO: handle error better lolz
+            return
+        
+        conn.send("What's your message?".encode(FORMAT))
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+
+        self.unsent_messages[recipient].append((sender, msg))
+    
+    def send_unsent_messages(self, conn, addr):
+        for recipient in self.unsent_messages:
+            messages = self.unsent_messages[recipient]
+            print(messages)
+            for message in messages:
+                if recipient in self.active_accounts:
+                    recipient_addr = self.active_accounts[recipient]
+                    if recipient_addr == addr:
+                        # Are currently in the thread that is the recipient's connection
+                        conn.send(message[0].encode(FORMAT))
+                        conn.send(" sends: ".encode(FORMAT))
+                        conn.send(message[1].encode(FORMAT))
+                        # TODO: confirm message has been received
+            # TODO: do this in a thread-safe way lmao
+            self.unsent_messages[recipient] = []
+        conn.send(NO_MORE_DATA.encode(FORMAT))
+
+    def handle_client(self, conn, addr):
+        print(f"[NEW CONNECTION] {addr} connected.")
+        logged_in = False
+
+        # establish username
+        if not logged_in:
+            conn.send("Enter 0 to register. Enter 1 to login.".encode(FORMAT))
+
+            msg_length = conn.recv(HEADER).decode(FORMAT)
+            if msg_length:
+                msg_length = int(msg_length)
+                action = conn.recv(msg_length).decode(FORMAT)
+
+            print(action)
+
+            if action == "0":
+                username = self.register_user(conn, addr)
+            elif action == "1":
+                username = self.login_user(conn, addr)
+            else:
+                conn.send("Invalid input.".encode(FORMAT))
+                # TODO: CHANGE THIS TO WHILE LOOP LATER LOL
+
+        # TODO: some protocol for all this shit
+        conn.send(LOGIN_MESSAGE.encode(FORMAT))
+
+        # TODO: we have to make this a background process somehow later??
+        self.send_unsent_messages(conn, addr)
+
+        # TODO: add here i forgot the specs
+        # conn.send("Enter 0 to send a message. Enter 1 to delete account.".encode(FORMAT))
+
+        # msg_length = conn.recv(HEADER).decode(FORMAT)
+        # if msg_length:
+        #     msg_length = int(msg_length)
+        #     action = conn.recv(msg_length).decode(FORMAT)
+        
+        self.record_chat_message(conn, addr)
+        
+        # connected = True
+        # while connected:
+        #     # depends on your protocol
+        #     msg_length = conn.recv(HEADER).decode(FORMAT)
+        #     if msg_length:
+        #         msg_length = int(msg_length)
+        #         msg = conn.recv(msg_length).decode(FORMAT)
+        #         if msg == DISCONNECT_MESSAGE:
+        #             connected = False
+        #         print(f"[{addr}] {msg}")
+        #         conn.send("Msg received".encode(FORMAT))
+
+        conn.close()
+            
+    # Notes: new thread for each client! (do we have to log them out?)
+    def start(self):
+        self.server.listen()
+        print(f"[LISTENING] Server is listening on {SERVER}")
+        while True:
+            conn, addr = self.server.accept()
+            thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+            thread.start()
+            print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+chat_server = ChatServer()
 print("[STARTING] server is starting...")
-start()
+chat_server.start()
