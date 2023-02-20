83cc6e01319dc4e568f2fec38fac4b213cc0a4cd
Alyssa <alyssahuang@college.harvard.edu>
2023-02-19 23:46:49 -0500

    Tests (not working)

--- wire_protocol/client_tests.py
+++ wire_protocol/client_tests.py
@@ -4,6 +4,10 @@ from new_client import ChatClient
 from commands import *
 from io import StringIO
 
+
+# TODO: make sure tests run even when server is up
+
+
 @pytest.fixture
 def receiver_sender_message_to_server():
     return PURPOSE + SEPARATOR + SEND_MESSAGE + SEPARATOR + \
@@ -65,27 +69,32 @@ def parsed_message_no_more_data():
         BODY: " "
     }
 
-
-def test_create_message(basic_message_to_server, receiver_sender_message_to_server):
+def test():
     client = ChatClient()
-    data = client.create_message(LOGIN, "username")
-    assert data == basic_message_to_server
+    msg = "!PURPOSE:/!NOTIFY/!LENGTH:/23/!BODY:/alyssa sends: hi dale 1!PURPOSE:/!NOTIFY/!LENGTH:/23/!BODY:/alyssa sends: hi dale 2!PURPOSE:/!NOTIFY/!LENGTH:/23/!BODY:/alyssa sends: hi dale 3!PURPOSE:/!NOMOREDATA/!LENGTH:/1/!BODY:/ "
+    data = client.parse_messages(msg, [])
+    assert data == ' '
 
-    data = client.create_message(SEND_MESSAGE, "message", "recipient", "sender")
-    assert data == receiver_sender_message_to_server
+# def test_create_message(basic_message_to_server, receiver_sender_message_to_server):
+#     client = ChatClient()
+#     data = client.create_message(LOGIN, "username")
+#     assert data == basic_message_to_server
+
+#     data = client.create_message(SEND_MESSAGE, "message", "recipient", "sender")
+#     assert data == receiver_sender_message_to_server
 
 
-def test_parse_message(
-    notify_message_from_server, no_more_data_message_from_server, 
-    parsed_message_notify, parsed_message_no_more_data, expected_stdout
-):
-    client = ChatClient()
-    with patch('sys.stdout', new = StringIO()) as mock_output:
-        data = client.parse_message(notify_message_from_server)
-        assert data == parsed_message_notify
-        assert mock_output.getvalue() == expected_stdout
+# def test_parse_message(
+#     notify_message_from_server, no_more_data_message_from_server, 
+#     parsed_message_notify, parsed_message_no_more_data, expected_stdout
+# ):
+#     client = ChatClient()
+#     with patch('sys.stdout', new = StringIO()) as mock_output:
+#         data = client.parse_message(notify_message_from_server)
+#         assert data == parsed_message_notify
+#         assert mock_output.getvalue() == expected_stdout
 
-    with patch('sys.stdout', new = StringIO()) as mock_output:
-        data = client.parse_message(no_more_data_message_from_server)
-        assert data == parsed_message_no_more_data
-        assert mock_output.getvalue() == ''
+#     with patch('sys.stdout', new = StringIO()) as mock_output:
+#         data = client.parse_message(no_more_data_message_from_server)
+#         assert data == parsed_message_no_more_data
+#         assert mock_output.getvalue() == ''
