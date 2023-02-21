a3d429b7748bc313b5258080a8294400e31271bb
Maegan Jong <maegan.jong@gmail.com>
2023-02-21 14:09:04 -0500

    README & requirements file for env

--- gRPC/commands.py
+++ gRPC/commands.py
@@ -2,7 +2,7 @@
 HEADER = 64
 PORT = 8500
 FORMAT = 'utf-8'
-SERVER = "dhcp-10-250-236-34.harvard.edu" # TODO: need to change later/have some way to make it dynamic
+SERVER = "dhcp-10-250-18-31.harvard.edu" # TODO: need to change later/have some way to make it dynamic
 ADDR = (SERVER, PORT)
 
 # Data Types
